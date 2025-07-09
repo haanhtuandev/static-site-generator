9a63346c67156234169a28ccd783072d2129d4ad
haanhtuandev <tuanha41105@gmail.com>
2025-07-09 21:52:15 +0700

    lesson 1  - website save progress

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from textnode import *
 import os
 import shutil
-import os
-import shutil
+
 
 def clear_directory_contents(path):
     for item in os.listdir(path):
@@ -13,16 +12,20 @@ def clear_directory_contents(path):
             shutil.rmtree(item_path)
 
 def copy_source(path, dest):
-    # clear_directory_contents(dest)
+    
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
+        print(item)
         if os.path.isfile(item_path) or os.path.islink(item_path):
             shutil.copy(item_path, dest)
-        # elif os.path.isdir(item_path):
-        #     copy_source(item_path, )
+        elif os.path.isdir(item_path):
+            dir_path = f"{dest}{item}/" 
+            os.mkdir(dir_path)
+            new_path = item_path + "/"
+            copy_source(new_path, dir_path)
 
 def main():
-    print("main")
-    copy_source("../static/", "../public/")
+    copy_source("static/", "public/")
 
+clear_directory_contents("public/")
 main()
